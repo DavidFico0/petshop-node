350b6c490c9d24b1b9e3a7ba25f42da159c4fac6
DavidFico0 <davidfico22@gmail.com>
2020-02-19 10:15:04 -0300

    Refatoração codigo da petsho-dh para node

--- index.js
+++ index.js
@@ -3,54 +3,75 @@ const petshop = require('./petshop');
 const url = require('url');
 
 const server = http.createServer((req, res)=> {
-    res.writeHead(200, { "Content-Type": "text/plain; charset=UTF-8"});
+    //res.writeHead(200, { "Content-Type": "text/plain; charset=UTF-8"});
     let urlCompleta = url.parse(req.url, true);
-    let queryString = url.queryString;
-    let rota = urlCompleta.pathname;
+    let queryString = urlCompleta.query; // parametros
+    let rota = urlCompleta.pathname; // ex: pets/add
 
     console.log(urlCompleta);
 
     switch(rota){
-        case '/home':
-        res.write('***** Bem vindo ao Pet Shop *****')
-        break;
+        case '/':
+            //Home e Menu do sistema
+            res.end("<h1>**** PET SHOP ****</h1><a href='/pets'>Lista pets</a> | <a href='/pets/adicionar'>Adicionar Pet</a> | <a href='/pets/buscapets'>Buscar pets</a>")
+            break;
+
+        case '/pets/adicionar' :
+            res.end(`<h1>** Adicionar Pet **</h1><a href='/'>Home</a> | <a href='/pets'>Lista pets</a> | <a href='/pets/adicionar'>Adicionar Pet</a> | <a href='/pets/buscapets'>Buscar pets</a><p>Insira o nome do pet:<form method='GET' action='/pets/add?'><input type="text" name="nome" placeholder='Nome pet...' /><br/> <input type='text' name='tipo' placeholder='Tipo...' /><br/><input type='text' name='raca' placeholder='Raca...' /><br/><input name='genero' type='text' placeholder='Genero...' /> <br/><input type='number' name='idade' placeholder='Idade...' /> <br/><input type='radio' name='vacinado' value='false'/>Nao Vaciando <input type='radio' name='vacinado' value='true'/>Vaciando<br/><input type='submit' value='Inserir' /></form></p>`)
+            break;
+        
         case '/pets' :
         // LISTA PETS
         let conteudo = petshop.listarPets();
         if(conteudo.length > 0){
             res.write(conteudo);
+            
         }
         else {
             res.write('Nenhum pet cadastrado');
         }
-        break;
+            break;
 
         case '/pets/add':
         // ADICIONA PET
         let novoPet = queryString;
-        
-        if(petshop.adicionarPet(novoPet)){
-            res.write(`${novoPet.nome} Adicionando Pet`)
-            console.log(novoPet.nome);
+        if (petshop.adicionarPet(novoPet)) {
+          res.write(`<p>${novoPet.nome} foi adicionado a nossa lista! - <a href='/pets'>Listar pets</a></p>`);
         } else {
-            res.write('Erro ao inserir novo pet');
+          res.write("Ops, algo deu errado!");
         }
-        break;
+            break;
+
+        case '/pets/buscapets' :
+                res.end(`<h1>** Buscar Pet **</h1><a href='/'>Home</a> | <a href='/pets'>Lista pets</a> | <a href='/pets/adicionar'>Adicionar Pet</a> | <a href='/pets/buscapets'>Buscar pets</a><p>Insira o nome do pet:<form method='GET' action='/pets/buscar?'><input type="text" name="nome" /> - <input type='submit' value='Buscar' /></form></p>`)
+                break;
 
         case '/pets/buscar':
         // Realiza busca pet
         let nomePet = queryString.nome;
         let petsEncontrados = petshop.buscarPet(nomePet);
-        if(petsEncontrados.length > 0){
-            res.write(`${petsEncontrados.length} pets com o nome ${nomePet}`)
+        if (petsEncontrados.length > 0) {
+          res.write(
+            `<a href='/'>Home</a> | <a href='/pets'>Lista pets</a> | <a href='/pets/adicionar'>Adicionar Pet</a> | <a href='/pets/buscapets'>Buscar pets</a><p>Encontramos ${petsEncontrados.length} pets com o nome ${nomePet}</p><p>Nome: ${nomePet}</p><p>Idade:${petsEncontrados[0].idade}</p><p>Vacinado: ${petsEncontrados[0].vacinado == 'true' ? 'S' : 'N' }</p><h1>**Servicos disponiveis**</h1><p><form method='GET' action='/pets/servicos'><input type="checkbox" name="servico[]" value="Banho">Banho <input type="checkbox" name="servico[]" value="Tosa">Tosa <input type="checkbox" name="servico[]" value="Corte de Unhas">Corte de Unhas <input type="checkbox" name="vacinado" value="true">Vacina <input type='hidden' name='petEncontrado' value='${petsEncontrados[0].nome}' /><input type='submit' value='Registrar'/></form></p>`
+          );
         } else {
-            res.write('Opst, nenhum pet cadastrado com esse nome!');
+          res.write("Ops, nenhum pet cadastrado com esse nome!");
         }
-        break;
+            break;
+      
+        case '/pets/servicos':
+        // ADICIONA SERVICOS PET
+          let json = JSON.parse(JSON.stringify(queryString));
+          let obj = Object.values(json)
+          let petEncontr = petshop.buscarPet(queryString.petEncontrado)
+          console.log(obj)
+          petshop.atenderPet(petEncontr[0], obj)
+          res.write("Sucesso! - <a href='/pets'>Listar pets</a></p> |" + obj);
+            break;
 
         default:
-        res.write('404 - Não encontrato')
-        break;
+        res.write('404 - Nao encontrato')
+            break;
     }
     res.end();
 }).listen(3000, 'localhost', ()=> {
--- petshop.js
+++ petshop.js
@@ -1,23 +1,44 @@
-let pets = [{nome: "doug" }, {nome: 'Costelinha'}, {nome: 'Bud'}]
+let pet = {
+    nome: '',
+    raca: '',
+    tipo: '',
+    genero: '',
+    idade: 0,
+    vacinado: false,
+    servicos: []
+}
+let pets = []
 
+//Cria uma tabela de pets
 let listarPets = () => {
-    let conteudo = "";
+    let conteudo = "<h1>**** Lista de Pets ****</h1><a href='/'>Home</a>" + " | <a href='/pets/adicionar'>Adicionar Pet</a> | <a href='/pets/buscapets'>Buscar pets</a><table style='width:100%; border: 1px solid black; border-collapse: collapse;'><tr style='border: 1px solid black; border-collapse: collapse;'><th>Nome</th><th>Tipo</th><th>Raca</th><th>Genero</th><th>Idade</th><th>Vacinado?</th><th>Servicos</th></tr>";
+    console.log(pets);
     for(let pet of pets){
-        conteudo += `
-        -----------------
-        Nome: ${pet.nome}
-        -----------------
-        `;
+        //Retorna data de nascimento do pet
+        let dataAtual = new Date()
+        let dataNascimento = dataAtual.getFullYear() - pet.idade;
+        //Retorna linhas da tabela
+        conteudo += `<tr style='border: 1px solid black; border-collapse: collapse;'><td>${pet.nome}</td><td>${pet.tipo}</td><td>${pet.raca}</td><td>${pet.genero}</td><td>${pet.idade}anos nasc:${dataNascimento}</td><td>${pet.vacinado == 'true' ? 'S' : 'N' }</td><td>${pet.servicos}</td></tr>`;
     }
+    conteudo += "</table>"
     return conteudo;
 }
 
+//Inseri novo pet
 let adicionarPet = novoPet => {
     return pets.push(novoPet);
 }
 
-let buscarPets = (nomePet) => {
-    return petsEncontrados = pets.filter(pet)
+//Busca por nome do pet
+const buscarPet = nomePet => {
+    let petsEncontrados = pets.filter(pet => pet.nome == nomePet);
+    return petsEncontrados;
+};
+
+const atenderPet = (pets, servicos) => {
+    pets[0] = servicos;
+    return servico(pets);
 }
 
-module.exports = { listarPets, adicionarPet, buscarPets };
+
+module.exports = { listarPets, adicionarPet, buscarPet, atenderPet };
